1f9f290c0ec115fd4745741d48c31bdc46d92470
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-20 21:54:58 -0500

    Setup a to z venv

--- az/another.py
+++ az/another.py
@@ -1,9 +1,23 @@
 import json
 from binance import Client
+import pandas as pd
+
+def get_api_essentals(data):
+    return data['api_key'], data['api_secret']
 
 with open('./settings.json', 'r') as file:
     data = json.load(file)
-    api_key = data['api_key']
-    api_secret = data['api_secret']
-    client = Client(api_key = api_key, api_secret = api_secret, tld = "us")
-    print(client.get_account()) # account details
+
+api_key, api_secret = get_api_essentals(data)
+
+client = Client(api_key=api_key, api_secret=api_secret, tld="us")
+account = client.get_account()
+balances = account["balances"]
+print(balances)
+df = pd.DataFrame(balances)
+df.info()
+
+df.free = pd.to_numeric(df.free, errors="coerce")
+df.locked = pd.to_numeric(df.locked, errors="coerce")
+df.loc[df.free > 0]
+client.get_asset_balance(asset="BTC")
