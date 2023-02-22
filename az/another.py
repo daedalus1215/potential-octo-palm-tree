6ef7caf2d89fa099bc12ca74dd563af7ee33387a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-21 21:43:02 -0500

    exploring more with bin api

--- az/another.py
+++ az/another.py
@@ -2,8 +2,10 @@ import json
 from binance import Client
 import pandas as pd
 
+
 def get_api_essentals(data):
     return data['api_key'], data['api_secret']
+
 
 with open('./settings.json', 'r') as file:
     data = json.load(file)
@@ -20,4 +22,17 @@ df.info()
 df.free = pd.to_numeric(df.free, errors="coerce")
 df.locked = pd.to_numeric(df.locked, errors="coerce")
 df.loc[df.free > 0]
-client.get_asset_balance(asset="BTC")
+print(client.get_asset_balance(asset="BTC"))
+print(client.get_asset_balance(asset="ETH"))
+float(client.get_asset_balance(asset="USD")["free"])
+
+print(client.get_symbol_ticker(symbol="BTCUSDT"))
+print(client.get_avg_price(symbol="BTCUSDT"))
+df = pd.DataFrame(client.get_all_tickers())
+print(df)
+print(df[df.symbol.str.contains("BTC")])
+print(df[df.symbol.str.contains("BTC") & df.symbol.str.contains("USD")])
+last24 = client.get_ticker(symbol="BTCUSDT")
+print(last24)
+
+print(float(last24["openPrice"]))
