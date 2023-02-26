f357a210b1f3beb80149b470aa9c2bc64d134fae
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-26 15:05:30 -0500

    Connect to the spot test network Grab the earliest orders from the spot test network

--- az/another.py
+++ az/another.py
@@ -18,3 +18,31 @@ client.create_test_order(symbol="BTCUSDT",
                          side="BUY",
                          type = "MARKET",
                          quantity = 1.0)
+
+
+def get_history(symbol, interval, start, end=None):
+    bars = client.get_historical_klines(symbol=symbol, interval=interval, start_str=start, end_str=end, limit=1000)
+
+    df = pd.DataFrame(bars)
+    df["Date"] = pd.to_datetime(df.iloc[:, 0], unit="ms")
+    df.columns = [
+        "Open Time",
+        "Open",
+        "High",
+        "Low",
+        "Close",
+        "Volume",
+        "Close Time",
+        "Quote Asset Volume",
+        "Number of Trades",
+        "Taker Buy Base Asset Volume",
+        "Taker Buy Quote Asset Volume",
+        "Ignore",
+        "Date"]
+
+    df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
+    df.set_index("Date", inplace=True)
+    for column in df.columns:
+        df[column] = pd.to_numeric(df[column], errors="coerce")
+
+    return df
